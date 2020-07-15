8244c502941b276b7bdab81bc503bf37fef99b8d
mark-scholes <innominatus15@outlook.com>
2020-07-15 12:05:10 +0100

    Added the ability to add a new item to the todo list

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import "./App.css";
 
 const App = () => {
   const [todos, setTodos] = useState(["Learn React", "boxvr workout"]);
+  const [input, setInput] = useState("");
+  const addTodo = () => {
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <input />
-      <button>Add todo</button>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add todo</button>
 
       <ul>
         {todos.map((todo) => {
